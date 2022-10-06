7f068932e27023647e3a42b3029f612e6d414959
mllrdev-kennrhey-miraflores <kennrhey.miraflores@mllrdev.com>
2022-10-07 01:32:57 +0800

    refactor: increase sample data

--- src/sampleData/testTableData.js
+++ src/sampleData/testTableData.js
@@ -28,6 +28,69 @@ const tableData = [
         "target":90,
         "actual":92,
         "points":26
+    },
+
+    {
+        "name":"Lauren Vickers 2",
+        "target":90,
+        "actual":95,
+        "points":69
+    },
+    {
+        "name":"Shazia Darby 2",
+        "target":80,
+        "actual":88,
+        "points":26
+    },
+    {
+        "name":"Zara Cain 2",
+        "target":85,
+        "actual":81,
+        "points":124
+    },
+    {
+        "name":"Clair Newton 2",
+        "target":80,
+        "actual":22,
+        "points":82
+    },
+    {
+        "name":"April Bruce Kieran 2",
+        "target":90,
+        "actual":92,
+        "points":26
+    },
+
+
+    {
+        "name":"Lauren Vickers 3",
+        "target":90,
+        "actual":95,
+        "points":69
+    },
+    {
+        "name":"Shazia Darby 3",
+        "target":80,
+        "actual":88,
+        "points":26
+    },
+    {
+        "name":"Zara Cain 3",
+        "target":85,
+        "actual":81,
+        "points":124
+    },
+    {
+        "name":"Clair Newton 3",
+        "target":80,
+        "actual":22,
+        "points":82
+    },
+    {
+        "name":"April Bruce Kieran 3",
+        "target":90,
+        "actual":92,
+        "points":26
     }
 ]
 
