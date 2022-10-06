0f4ec7f8a9e7bcdb1c39c1fdfc689a9f8f7accd3
mllrdev-kennrhey-miraflores <kennrhey.miraflores@mllrdev.com>
2022-10-07 00:30:44 +0800

    refactor: fix indentation

--- src/sampleData/testTableData.js
+++ src/sampleData/testTableData.js
@@ -1,33 +1,33 @@
 const tableData = [
     {
-    "name":"Lauren Vickers",
-    "target":90,
-    "actual":95,
-    "points":69
+        "name":"Lauren Vickers",
+        "target":90,
+        "actual":95,
+        "points":69
     },
     {
-    "name":"Shazia Darby",
-    "target":80,
-    "actual":88,
-    "points":26
+        "name":"Shazia Darby",
+        "target":80,
+        "actual":88,
+        "points":26
     },
     {
-    "name":"Zara Cain",
-    "target":85,
-    "actual":81,
-    "points":124
+        "name":"Zara Cain",
+        "target":85,
+        "actual":81,
+        "points":124
     },
     {
-    "name":"Clair Newton",
-    "target":80,
-    "actual":22,
-    "points":82
+        "name":"Clair Newton",
+        "target":80,
+        "actual":22,
+        "points":82
     },
     {
-    "name":"April Bruce Kieran",
-    "target":90,
-    "actual":92,
-    "points":26
+        "name":"April Bruce Kieran",
+        "target":90,
+        "actual":92,
+        "points":26
     }
 ]
 
